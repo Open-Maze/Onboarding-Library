a78eade2aef165471c5943528a66eb0c70002b9a
RafSchapendonk <rafschapendonk@outlook.com>
2024-04-09 12:55:46 +0200

    fix: story is able to take components

--- lib/OnboardingProvider/OnboardingProvider.stories.tsx
+++ lib/OnboardingProvider/OnboardingProvider.stories.tsx
@@ -1,19 +1,44 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import Step from '../Components/OnboardingSteps/Step';
 import OnboardingProvider from './OnboardingProvider';
+
 const meta: Meta<typeof OnboardingProvider> = {
   component: OnboardingProvider,
 };
 
 export default meta;
-
 type Story = StoryObj<typeof OnboardingProvider>;
 
-export const Default: Story = {
-  args: {},
+export const Primary: Story = {
+  render: () => <OnboardingProvider />,
 };
 
 export const WithChildren: Story = {
-  args: {
-    children: <div>test</div>,
-  },
+  render: () => (
+    <OnboardingProvider>
+      <Step />
+    </OnboardingProvider>
+  ),
 };
+
+// const meta: Meta<typeof OnboardingProvider> = {
+//   component: OnboardingProvider,
+// };
+
+// export default meta;
+
+// type Story = StoryObj<typeof OnboardingProvider>;
+
+// export const Default: Story = {
+//   args: {},
+// };
+
+// export const WithChildren: Story = {
+//   args: {
+//     children: (
+//       <div>
+//         test<div>test</div>
+//       </div>
+//     ),
+//   },
+// };
