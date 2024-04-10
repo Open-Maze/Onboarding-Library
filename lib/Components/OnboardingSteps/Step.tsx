8a1ba0528a161a413c00db659586bfa28a248a94
RafSchapendonk <rafschapendonk@outlook.com>
2024-04-10 16:24:31 +0200

    chore: removed styling to adhere to user story definition of done

--- lib/Components/OnboardingSteps/Step.tsx
+++ lib/Components/OnboardingSteps/Step.tsx
@@ -8,7 +8,7 @@ export default function Step({ stepProps }: { stepProps: StepProps }) {
   return (
     <div className="bg-white max-w-[300px] justify-between min-h-[400px] w-full flex flex-col p-[4px]">
       <p>{stepProps.content}</p>
-      <button onClick={stepProps.nextStep} className="bg-blue-500">
+      <button onClick={stepProps.nextStep} className="border border-black">
         {stepProps.lastStep ? 'Close' : 'Next'}
       </button>
     </div>
