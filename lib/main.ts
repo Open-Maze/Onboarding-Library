8e65487a3021d921f2f91740089ee7f112827e05
RafSchapendonk <rafschapendonk@outlook.com>
2024-05-02 10:20:32 +0200

    fix: removed old code

--- lib/main.ts
+++ lib/main.ts
@@ -1,8 +1,18 @@
 import 'material-symbols';
 import Button from './Components/Button';
 import OnboardingProvider from './OnboardingProvider/OnboardingProvider';
-import ProductTour from './OnboardingProvider/ProductTour';
 import Step from './OnboardingSteps/Step';
-import Tooltip from './OnboardingSteps/Tooltip';
+import {
+  ProductTour,
+  Tooltip,
+  TooltipContent,
+} from './OnboardingSteps/Tooltip';
 import './tailwind.css';
-export { Button, OnboardingProvider, ProductTour, Step, Tooltip };
+export {
+  Button,
+  OnboardingProvider,
+  ProductTour,
+  Step,
+  Tooltip,
+  TooltipContent,
+};
