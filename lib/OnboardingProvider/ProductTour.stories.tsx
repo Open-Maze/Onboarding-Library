cebe4d35c32d620866190c81d33126cdf5ccd62b
RafSchapendonk <rafschapendonk@outlook.com>
2024-05-08 16:14:58 +0200

    feat: remake producttour component to handle current onboarding components

--- lib/OnboardingProvider/ProductTour.stories.tsx
+++ lib/OnboardingProvider/ProductTour.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import WebsiteWireframe from '../Components/WebsiteWireframe.tsx';
+import Popover from '../OnboardingSteps/Popover.tsx';
 import ProductTour from './ProductTour.tsx';
 
 const meta: Meta<typeof ProductTour> = {
@@ -13,17 +13,45 @@ export const Primary: Story = {
   parameters: {
     layout: 'fullscreen',
   },
-  render: () => <ProductTour></ProductTour>,
-};
-
-export const ProductTourOnTemplate: Story = {
-  parameters: {
-    layout: 'fullscreen',
-  },
   render: () => (
     <>
-      <ProductTour></ProductTour>
-      <WebsiteWireframe />
+      <div className="flex justify-center items-center h-screen w-auto">
+        <div id="anchor" className="flex w-12 h-12 bg-red-500">
+          anchor
+        </div>
+      </div>
+      <ProductTour>
+        <Popover
+          target={'anchor'}
+          targetSpacing={8}
+          placement={'top'}
+          text="Lorem ipsum 1"
+        />
+        <Popover
+          target={'anchor'}
+          targetSpacing={16}
+          placement={'left'}
+          text="Lorem ipsum 2"
+        />
+        <Popover
+          target={'anchor'}
+          targetSpacing={24}
+          placement={'bottom'}
+          text="Lorem ipsum 3"
+        />
+      </ProductTour>
     </>
   ),
 };
+
+// export const ProductTourOnTemplate: Story = {
+//   parameters: {
+//     layout: 'fullscreen',
+//   },
+//   render: () => (
+//     <>
+//       <ProductTour></ProductTour>
+//       <WebsiteWireframe />
+//     </>
+//   ),
+// };
--- lib/OnboardingProvider/ProductTour.tsx
+++ lib/OnboardingProvider/ProductTour.tsx
@@ -1,28 +1,37 @@
-import React, { useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 
-interface ProductTourProps {
-  children?: React.ReactNode;
+interface InterfaceProductTour {
+  children: Array<ReactElement<unknown>>;
 }
 
-export default function ProductTour({ children }: ProductTourProps) {
+export default function ProductTour({ ...props }: InterfaceProductTour) {
   const [index, setIndex] = useState(0);
 
+  function handleClick() {
+    setIndex(index + 1);
+    console.log(index);
+  }
+
   const renderChildren = () => {
-    return React.Children.map(children, (child, index) => {
-      return React.cloneElement(child as JSX.Element, {
-        filledButtonFunc: () => {
-          setIndex(index + 1);
-          console.log('filledButton clicked' + index);
-        },
-        textButtonFunc: () => {
-          if (index !== 0) {
-            setIndex(index - 1);
-            console.log('textButton clicked' + index);
-          }
-        },
-        index,
-      });
+    return React.Children.map(props.children, (child) => {
+      return (
+        <>
+          <button className="absolute top-0 left-0 z-100" onClick={handleClick}>
+            next {index}
+          </button>
+          {child}
+        </>
+      );
     });
   };
-  return <div>{renderChildren()?.[index]}</div>;
+
+  if (props.children) {
+    return <>{renderChildren()?.[index]}</>;
+  } else {
+    console.error(
+      'ProductTour component must have child components from the onboarding library'
+    );
+    console.log(props.children);
+  }
+  return;
 }
