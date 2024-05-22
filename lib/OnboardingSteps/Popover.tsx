4198806cfdc0915e7b230e6ade872819d219c50f
RafSchapendonk <rafschapendonk@outlook.com>
2024-05-22 09:25:36 +0200

    refactor: listen to popover size changes

--- lib/OnboardingSteps/Popover.tsx
+++ lib/OnboardingSteps/Popover.tsx
@@ -36,10 +36,6 @@ export default function Popover({
 }: PopoverOptions) {
   const [style, setStyle] = useState({ top: -1, left: -1 });
   const [rectangle, setRectangle] = useState({ top: 0, left: 0 });
-  const [popoverRectangle, setPopoverRectangle] = useState({
-    height: 0,
-    width: 0,
-  });
 
   const popoverRef = useRef<HTMLDivElement>(null);
 
@@ -52,26 +48,13 @@ export default function Popover({
     }
   };
 
-  const setPopoverRectValues = () => {
-    if (!popoverRef.current) {
-      return;
-    }
-    const height = popoverRef.current.getBoundingClientRect().height;
-    const width = popoverRef.current.getBoundingClientRect().width;
-
-    if (height != popoverRectangle.height || width != popoverRectangle.width) {
-      setPopoverRectangle({ height, width });
-    }
-  };
-
   const popoverPosition = () => {
-    setRectValues();
-    setPopoverRectValues();
-
-    if (targetRef.current instanceof HTMLElement) {
+    if (targetRef.current instanceof HTMLElement && popoverRef.current) {
       const popoverRect =
-        popoverRef.current?.getBoundingClientRect() || new DOMRect();
+        popoverRef.current.getBoundingClientRect() || new DOMRect();
       const targetRect = targetRef.current.getBoundingClientRect();
+
+      setRectValues();
 
       let top = 0;
       let left = 0;
@@ -88,10 +71,7 @@ export default function Popover({
           break;
         case 'top':
           top =
-            rectangle.top +
-            globalThis.scrollY -
-            popoverRectangle.height +
-            targetSpacing;
+            rectangle.top + window.scrollY - popoverRect.height + targetSpacing;
           left = rectangle.left + targetRect.width / 2 - popoverRect.width / 2;
 
           break;
@@ -118,99 +98,83 @@ export default function Popover({
           break;
       }
 
-      console.log(
-        `top: ${rectangle.top}, popoverRect: ${popoverRectangle.height}, scrollY: ${globalThis.scrollY}, targetSpacing: ${targetSpacing}`
-      );
       setStyle({ top, left });
-    } else {
-      console.error(`Popover target ref not found`);
+    } else if (!popoverRef.current) {
+      console.error(`Popover ref not found`);
+    } else if (!targetRef.current) {
+      console.error(`Target ref not found`);
     }
   };
 
   useEffect(() => {
-    console.log('useeffect 1');
     if (targetRef.current) {
       const observer = new ResizeObserver(() => {
         setRectValues();
       });
 
-      observer.observe(targetRef.current);
+      observer.observe(document.body);
 
       return () => observer.disconnect();
     }
   }, []);
 
   useEffect(() => {
-    if (!popoverRef.current) {
-      return;
+    if (popoverRef.current) {
+      const observer = new ResizeObserver(() => {
+        popoverPosition();
+      });
+
+      observer.observe(popoverRef.current);
+
+      return () => observer.disconnect();
     }
-    const observer = new ResizeObserver(() => {
-      setPopoverRectValues();
-    });
-
-    observer.observe(popoverRef.current);
-
-    return () => observer.disconnect();
   }, []);
 
   useEffect(() => {
-    console.log('useeffect 2');
-    console.log(rectangle);
     popoverPosition();
-  }, [targetRef, rectangle, popoverRectangle]);
+  }, [targetRef, rectangle]);
 
   useEffect(() => {
-    console.log('useeffect 3');
-    console.log(rectangle);
     popoverPosition();
   }, []);
 
   return (
     <>
-      {style.top != -1 && style.left != -1 && (
-        <div
-          ref={popoverRef}
-          style={{
-            top: `${style.top}px`,
-            left: `${style.left}px`,
-          }}
-          className="ol-max-w-[312px] ol-absolute ol-bg-gray ol-px-4 ol-z-100 ol-shadow-md ol-rounded-xl"
-        >
-          <div className="ol-pt-3 ol-pb-2 ol-gap-y-1 ol-flex ol-flex-col">
-            {children}
-            {icon && (
-              <span className={`material-symbols-${iconStyle}`}>{icon}</span>
-            )}
-            {title && <h2>{title}</h2>}
-            {image && <img src={image} className="ol-bg-gray-dark"></img>}
-            {text && <div>{text}</div>}
-            {currentStep &&
-              totalSteps &&
-              textButtonFunc &&
-              filledButtonFunc && (
-                <div className="ol-flex ol-flex-row ol-items-center ol-justify-between">
-                  <div className="ol-text-gray-dark">
-                    {currentStep} of {totalSteps}
-                  </div>
-                  <div className="ol-flex ol-flex-row ol-gap-x-2.5">
-                    <TextButton
-                      text={'Previous'}
-                      onClickFunc={textButtonFunc || (() => {})}
-                    ></TextButton>
-                    <Button
-                      text={'Next'}
-                      onClickFunc={filledButtonFunc || (() => {})}
-                    ></Button>
-                    <Button
-                      text={'update'}
-                      onClickFunc={popoverPosition || (() => {})}
-                    ></Button>
-                  </div>
-                </div>
-              )}
-          </div>
+      <div
+        ref={popoverRef}
+        style={{
+          top: `${style.top}px`,
+          left: `${style.left}px`,
+        }}
+        className="ol-max-w-[312px] ol-absolute ol-bg-gray ol-px-4 ol-z-100 ol-shadow-md ol-rounded-xl"
+      >
+        <div className="ol-pt-3 ol-pb-2 ol-gap-y-1 ol-flex ol-flex-col">
+          {children}
+          {icon && (
+            <span className={`material-symbols-${iconStyle}`}>{icon}</span>
+          )}
+          {title && <h2>{title}</h2>}
+          {image && <img src={image} className="ol-bg-gray-dark"></img>}
+          {text && <div>{text}</div>}
+          {currentStep && totalSteps && textButtonFunc && filledButtonFunc && (
+            <div className="ol-flex ol-flex-row ol-items-center ol-justify-between">
+              <div className="ol-text-gray-dark ol-flex ol-flex-row ol-flex-nowrap">
+                {currentStep} of {totalSteps}
+              </div>
+              <div className="ol-flex ol-flex-row ol-gap-x-2.5">
+                <TextButton
+                  text={'Previous'}
+                  onClickFunc={textButtonFunc || (() => {})}
+                ></TextButton>
+                <Button
+                  text={'Next'}
+                  onClickFunc={filledButtonFunc || (() => {})}
+                ></Button>
+              </div>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 }
