cc5a76275d427d87ad45fff71378614e73ad9b72
RafSchapendonk <rafschapendonk@outlook.com>
2024-05-27 09:39:28 +0200

    chore: removed unnecesary code

--- lib/Components/ProductTourNavigation.tsx
+++ lib/Components/ProductTourNavigation.tsx
@@ -15,25 +15,23 @@ const ProductTourNavigation = ({
   textButtonFunc,
 }: ProductTourNavigationProps) => (
   <>
-    {currentStep && totalSteps && textButtonFunc && filledButtonFunc && (
-      <div className="ol-flex ol-flex-row ol-items-center ol-justify-between">
-        <div className="ol-text-gray-dark ol-pr-4 ol-text-nowrap">
-          {currentStep + ' of ' + totalSteps}
-        </div>
-        <div className="ol-flex ol-flex-row ol-gap-x-2.5">
-          {currentStep > 1 && (
-            <TextButton
-              text={'Previous'}
-              onClickFunc={textButtonFunc || (() => {})}
-            ></TextButton>
-          )}
-          <Button
-            text={currentStep >= totalSteps ? 'Finish' : 'Next'}
-            onClickFunc={filledButtonFunc || (() => {})}
-          ></Button>
-        </div>
+    <div className="ol-flex ol-flex-row ol-items-center ol-justify-between">
+      <div className="ol-text-gray-dark ol-pr-4 ol-text-nowrap">
+        {currentStep + ' of ' + totalSteps}
       </div>
-    )}
+      <div className="ol-flex ol-flex-row ol-gap-x-2.5">
+        {currentStep > 1 && (
+          <TextButton
+            text={'Previous'}
+            onClickFunc={textButtonFunc || (() => {})}
+          ></TextButton>
+        )}
+        <Button
+          text={currentStep >= totalSteps ? 'Finish' : 'Next'}
+          onClickFunc={filledButtonFunc || (() => {})}
+        ></Button>
+      </div>
+    </div>
   </>
 );
 
