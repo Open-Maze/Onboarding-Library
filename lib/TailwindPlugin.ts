59b4759423aa9d2b6fb6e580c4bbb4361ef8086b
RafSchapendonk <rafschapendonk@outlook.com>
2024-05-28 15:21:33 +0200

    refactor: made creation of colorutilities its own function

--- lib/TailwindPlugin.ts
+++ lib/TailwindPlugin.ts
@@ -1,41 +1,54 @@
 import plugin from 'tailwindcss/plugin';
-import { CSSRuleObject } from 'tailwindcss/types/config';
+import { CSSRuleObject, CustomThemeConfig } from 'tailwindcss/types/config';
 
 interface ColorConfig {
   colors?: Record<string, string>;
+  spacing?: Record<string, string>;
+}
+
+function createColorUtilities(
+  theme: <
+    TDefaultValue = Partial<
+      CustomThemeConfig & { extend: Partial<CustomThemeConfig> }
+    >,
+  >(
+    path?: string | undefined,
+    defaultValue?: TDefaultValue | undefined
+  ) => TDefaultValue,
+  options: ColorConfig
+) {
+  const colors = { ...theme('colors'), ...options.colors };
+
+  const newColorUtilities = Object.keys(colors).reduce(
+    (acc, key) => {
+      acc[`.bg-${key}`] = { backgroundColor: colors[key] };
+      acc[`.text-${key}`] = { color: colors[key] };
+      acc[`.border-${key}`] = { borderColor: colors[key] };
+      return acc;
+    },
+    {} as Record<string, CSSRuleObject>
+  );
+
+  return newColorUtilities;
 }
 
 const colorConfigPlugin = plugin.withOptions<ColorConfig>(
   (options) => {
     return ({ addUtilities, theme }) => {
-      const userColors = options?.colors || {};
-      const colors = { ...theme('colors'), ...userColors } as Record<
-        string,
-        string
-      >;
-
-      const newUtilities = Object.keys(colors).reduce(
-        (acc, key) => {
-          acc[`.bg-${key}`] = { backgroundColor: colors[key] };
-          acc[`.text-${key}`] = { color: colors[key] };
-          acc[`.border-${key}`] = { borderColor: colors[key] };
-          return acc;
-        },
-        {} as Record<string, CSSRuleObject>
-      );
-
-      addUtilities(newUtilities);
+      const newColorUtilities = createColorUtilities(theme, options);
+      addUtilities(newColorUtilities);
     };
   },
   (options) => {
     return {
       theme: {
         extend: {
-          colors: options?.colors || {
-            // Default colors can be defined here
-            primary: '#1da1f2',
-            secondary: '#14171a',
-            danger: '#e0245e',
+          colors: {
+            primary: options.colors?.primary || '#8c1cec',
+            secondary: options.colors?.secondary || '#39cfe8',
+            gray: options.colors?.gray || '#E6E6E6',
+            'gray-dark': options.colors?.['gray-dark'] || '#797979',
+            background: options.colors?.background || '#F4F4F4',
           },
         },
       },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const colorConfigPlugin = require('./lib/TailwindPlugin.ts').default;
+import colorConfigPlugin from './lib/TailwindPlugin.ts';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -13,15 +13,6 @@ export default {
         100: '100',
         99: '99',
       },
-      colors: {
-        gray: {
-          DEFAULT: '#E6E6E6',
-          dark: '#797979',
-        },
-        primary: '#8c1cec',
-        secondary: '#39cfe8',
-        background: '#F4F4F4',
-      },
       fontFamily: {
         sans: ['Poppins'],
         heading: ['Manrope'],
@@ -33,8 +24,9 @@ export default {
       colors: {
         primary: '#fcba03',
         secondary: '#00ff00',
-        danger: '#0000ff',
-        customColor: '#f0f0f0',
+        background: '#3a5da0',
+        "gray-dark": '#333333',
+
       },
     }),
   ],
