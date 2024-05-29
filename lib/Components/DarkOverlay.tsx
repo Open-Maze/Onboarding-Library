d32738e892aee52c4af7e3cfa303d1976f06af88
RafSchapendonk <rafschapendonk@outlook.com>
2024-05-29 17:52:07 +0200

    feat: created an overlay element

---
+++ lib/Components/DarkOverlay.tsx
@@ -0,0 +1,6 @@
+const DarkOverlay = () => {
+  return (
+    <div className="ol-top-0 ol-left-0 ol-bg-black ol-opacity-50 ol-absolute ol-h-screen ol-z-98 ol-w-screen"></div>
+  );
+};
+export default DarkOverlay;
