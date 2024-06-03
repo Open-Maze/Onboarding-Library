6badf99615166f93dffca48ddd41b5a5fc7e8de2
RafSchapendonk <rafschapendonk@outlook.com>
2024-06-03 12:06:27 +0200

    feat: basic set up for popup component

--- lib/OnboardingSteps/Popover.tsx
+++ lib/OnboardingSteps/Popover.tsx
@@ -1,100 +1,7 @@
 'use client';
-import { ReactElement, useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import DarkOverlay from '../Components/DarkOverlay';
-
-/**
- * Interface for configuring the options of a popover component.
- */
-interface PopoverOptions {
-  /**
-   * A reference to the HTML element that the popover is attached to.
-   * Used to position the popover relative to the target element.
-   * @type {React.RefObject<HTMLElement>}
-   */
-  targetRef: React.RefObject<HTMLElement>;
-
-  /**
-   * The spacing (in pixels) between the target element and the popover.
-   * Controls how far the popover is from the target element.
-   * @type {number}
-   */
-  targetSpacing: number;
-
-  /**
-   * Specifies the position of the popover relative to the target element.
-   * The popover can be placed on the top, bottom, left, or right of the target element.
-   * @type {'top' | 'bottom' | 'left' | 'right'}
-   */
-  placement: 'top' | 'bottom' | 'left' | 'right';
-
-  /**
-   * The style of the icon displayed in the popover based on material symbols. Optional unless icon is defined.
-   * Can be 'outlined', 'rounded', or 'sharp'.
-   * @type {'outlined' | 'rounded' | 'sharp'}
-   */
-  iconStyle?: 'outlined' | 'rounded' | 'sharp';
-
-  /**
-   * The name of the icon to be displayed in the popover based on material symbols. Optional unless iconStyle is defined.
-   * Allows for an icon to be shown alongside the popover content.
-   * @type {string}
-   */
-  icon?: string;
-
-  /**
-   * The title text displayed at the top of the popover. Optional.
-   * Used to give a brief heading or description.
-   * @type {string}
-   */
-  title?: string;
-
-  /**
-   * The URL of an image to be displayed in the popover. Optional.
-   * Can be used to include visual content in the popover.
-   * @type {string}
-   */
-  image?: string;
-
-  /**
-   * The main text content of the popover. Optional.
-   * Used for displaying detailed information or instructions.
-   * @type {string}
-   */
-  text?: string;
-
-  /**
-   * React elements to be rendered inside the popover. Optional.
-   * Allows for complex content, including other components, to be included in the popover.
-   * @type {React.ReactElement}
-   */
-  children?: ReactElement;
-
-  /**
-   * Custom CSS properties provided by the product tour component.
-   * Used to override styling of the popover.
-   * @type {React.CSSProperties}
-   */
-  style?: React.CSSProperties;
-
-  /**
-   * A boolean value that determines whether the popover is part of a product tour.
-   * @type {boolean}
-   * @default false
-   */
-  productTour?: false;
-
-  /**
-   * A boolean value that determines whether the popover is visible or hidden.
-   * @type {boolean}
-   */
-  visible?: boolean;
-
-  /**
-   * A React element for navigation controls within the popover provided by the product tour component
-   * @type {React.ReactElement}
-   */
-  navigation?: ReactElement;
-}
+import { PopoverOptions } from '../types';
 
 /**
  * Popover component that displays a popover with various options.
@@ -295,7 +202,7 @@ export default function Popover({
                 ></img>
               </div>
             )}
-            {text && <div className="ol-text-lg">{text}</div>}
+            {text && <p className="ol-text-lg">{text}</p>}
             {navigation}
           </div>
         </div>
---
+++ lib/OnboardingSteps/Popup.tsx
@@ -0,0 +1,48 @@
+import ButtonPrimary from '../Components/ButtonPrimary';
+import DarkOverlay from '../Components/DarkOverlay';
+import { PopupOptions } from '../types';
+
+export default function Popup({
+  title,
+  text,
+  image,
+  children,
+  navigation,
+  productTour,
+}: PopupOptions) {
+  return (
+    <>
+      {!productTour && <DarkOverlay />}
+
+      <div className="ol-h-screen ol-flex ol-items-center ol-justify-center">
+        <div className="ol-bg-background ol-z-41 ol-relative ol-p-5 ol-rounded-3xl ol-max-w-[664px] ol-flex ol-flex-col ol-gap-y-4">
+          <span
+            onClick={function (): void {}}
+            className="material-symbols-outlined hover:ol-cursor-pointer ol-h-5 ol-w-5 ol-text-gray-dark hover:ol-text-secondary ol-text-center ol-content-center ol-absolute ol-top-2.5 ol-right-2.5"
+          >
+            close_small
+          </span>
+          {title && <h2 className="ol-text-4xl">{title}</h2>}
+          {image && (
+            <div className="ol-flex ol-justify-center">
+              <img
+                src={image}
+                className="ol-object-contain w-full h-auto"
+              ></img>
+            </div>
+          )}
+          {text && <p className="ol-text-lg">{text}</p>}
+          {children}
+          {navigation ? (
+            navigation
+          ) : (
+            <ButtonPrimary
+              text={'close pop up message'}
+              onClickHandler={function (): void {}}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
